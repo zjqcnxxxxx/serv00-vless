795f9d01a7f48f5ea985cde5f1492ae5092c9b8f
peggypapa <54925028+zjqcnxxxxx@users.noreply.github.com>
2024-06-25 00:13:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
 
 const uuid = (process.env.UUID || '37a0bd7c-8b9f-4693-8916-bd1e2da0a817').replace(/-/g, '');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 53554;
 
 const wss = new WebSocket.Server({ port }, logcb('listen:', port));
 
